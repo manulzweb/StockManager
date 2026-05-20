194332b7043b6eaa1de59bf9aebd8d46c8b5a504
manulzweb <manuelyt212@gmail.com>
2026-05-20 17:24:40 -0500

    Fixed the color stock error

--- client/src/shared/utils/stockHelpers.js
+++ client/src/shared/utils/stockHelpers.js
@@ -1,7 +1,7 @@
 export const obtenerColorPorStock = (stock) => {
     const COLORES_STOCK = [
-        { limite: 5, estilos: 'bg-(--danger-bg) text-(--danger-text) border-(--danger-border)' },
-        { limite: 10, estilos: 'bg-(--warning-bg) text-(--warning-text) border-(--warning-border)' }
+        { limite: 6, estilos: 'bg-(--danger-bg) text-(--danger-text) border-(--danger-border)' },
+        { limite: 11, estilos: 'bg-(--warning-bg) text-(--warning-text) border-(--warning-border)' }
     ]
 
     let primerValido = COLORES_STOCK.find(item => stock < item.limite)
